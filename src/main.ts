43afdcf78ba67173694bccc7a4e5f308c69dd015
Tufiale <tufiale@gmail.com>
2023-08-01 16:42:06 +0100

    Got numbers on disaply

--- src/main.ts
+++ src/main.ts
@@ -23,12 +23,51 @@ import "./style.scss";
 
 // TRY AGAIN??
 
-let display = document.querySelector(".display");
+// let display = document.querySelector(".display");
 
-let buttons = Array.from(document.querySelectorAll(".calculator__button"));
+// let buttons = Array.from(document.querySelectorAll(".calculator__button"));
 
-buttons.map((button) => {
-  button.addEventListener("click", (e) => {
-    console.log("clicked");
+// buttons.map((button) => {
+//   button.addEventListener("click", () => {
+//     console.log("clicked");
+//   });
+// });
+
+// Define the variable to hold the current display value
+let currentDisplayValue: string = "0";
+
+// Function to update the display with the current value
+function updateDisplay() {
+  const displayElement = document.getElementById("display") as HTMLInputElement;
+  displayElement.value = currentDisplayValue;
+}
+
+// Function to handle button clicks
+function handleButtonClick(event: Event) {
+  const target = event.target as HTMLButtonElement;
+  const buttonValue = target.value || target.innerText;
+
+  if (buttonValue === "AC") {
+    currentDisplayValue = "0";
+  } else if (buttonValue === "=") {
+    // Evaluate the expression and update the display
+    currentDisplayValue = currentDisplayValue.toString();
+  } else {
+    // Append the clicked button value to the current display value
+    currentDisplayValue += buttonValue;
+  }
+
+  // Update the display with the current value
+  updateDisplay();
+}
+
+// Function to add click event listeners to all buttons
+function setupButtonListeners() {
+  const buttons = document.querySelectorAll(".calculator__button");
+  buttons.forEach((button) => {
+    button.addEventListener("click", handleButtonClick);
   });
-});
+}
+
+// Call the setupButtonListeners function after the DOM has loaded
+document.addEventListener("DOMContentLoaded", setupButtonListeners);
